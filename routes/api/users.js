d76b6f019e533c552d98da56319d130f6e86c7f5
Avishka Indula <avishkainduladev@gmail.com>
2022-12-23 19:54:25 +0530

    Implementing JWT
    => we wanna return a web token once a user is registered
    *  because we wanna use that token to authenticate
    *  users to access protected routes.
    => jwt.io - breaks down what is in a JSON web token
    => a JSON web token is broken up to 3 parts
    => the first part shows the token type and the algorithm
    => the second part is the payload. That's the data we wanna
    *  send within the token. We wanna send the users id as the payload.
    => So that we can identify that user who just logged in.
    => import jsonwebtoken to users.js
    => creating const payload and jwt.sign in users.js
    => creating jwtSecret in default.json file.
    => import config to users.js
    => pass "jwtSecret" to jwt.sign()
    => configure jwt.sign()
    => res.json({ token });
    *  This will send the jwt token as the json response
    => This token includes the user.id we defined on the payload.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const config = require("config");
 
 // const { check, validationResult } = require("express-validator/check");
 // This throws a warning telling
@@ -47,7 +49,9 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       // Get users gravatar
@@ -76,7 +80,24 @@ router.post(
       // Return jsonwebtoken
       // In the front-end when the user registers we wanna get him logged in right away
       // so in order to get logged in we need to have that token
-      res.send("User registered");
+      // res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+          // This will send the jwt token as the json response
+          // This token includes the user.id we defined on the payload.
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
