14e0804713c2540e9f0afe7db579f9529b094f37
Avishka Indula <avishkainduladev@gmail.com>
2022-12-24 22:58:14 +0530

    Create & Update Profile Routes
    => now we're gonna create the route for creating and updating the
    *  profile in profile.js file.
    => const { check, validationResult } = require("express-validator");
    => creating router.post("/");
    => adding auth as a parameter to the route.
    => adding [check()] validation as parameters for status and skills inputs
    => --------------------------------------------------------------------------
    => sending a post request to http://localhost:5000/api/profile/ from postman
    => saving the token of test@testing.com and content type : application/json
    *  as a preset.
    => Now if we send this request, it throws an error telling "Status is required"
    *  and "Skills is required". Skills and Status is mandatory but we can skip
    *  the other data when we create the user profile.
    => adding { "status": "Developer", "skills": "NodeJS, React,Flutter" } as body
    *  json data of the post request. Those skills should be added comma separated
    *  and those values will be extracted and added to an array.
    => we can add skills.split(",").map() in the post route to achieve that.
    => --------------------------------------------------------------------------
    => creating properties inside the profileFields and assign to them the extracted
    *  data from the post request body.
    => creating a try catch block and add to it the logic to insert or update the
    *  extracted data to the database.
    => now we can send a post request to http://localhost:5000/api/profile/ with
    *  all data fields and this works as expected. updating data also works.
    => and those data is saved on the database as expected.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -37,5 +38,97 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Public
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+    // We're gonna pull all this stuff out from the request body.
+    // But we need to make sure this field are added before send them to
+    // the database. We can use if checks in below for that.
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    // This user.id will be extracted from the jwt web token sent along this post request.
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+      // This .split() convert the string to an array
+      // It take in a delimiter which is gonna be a ","
+      // This will split the skills separated by commas and add them as
+      // separate array values.
+    }
+
+    // console.log(profileFields.skills);
+    // JSON data sent => { "skills": "NodeJS, React,Flutter" }
+    // console output => [ 'NodeJS', 'React', 'Flutter' ]
+
+    // Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      // Update
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        // This will update the existing profile data of a user.
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields);
+      // This will create a new instance of Profile.
+      await profile.save();
+      // This will save the user profile data to the database.
+      res.json(profile);
+    } catch {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
