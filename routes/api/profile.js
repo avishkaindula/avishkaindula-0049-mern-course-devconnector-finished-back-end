16cfe97a30dad5967b879877bf99ad37a3e51519
Avishka Indula <avishkainduladev@gmail.com>
2022-12-24 16:20:34 +0530

    Get Current User Profile
    => creating a get route to get the profile in profile.js
    => router.get("/me")
    => importing auth.js and add auth, as a parameter to that route
    => import Profile and User models and create the necessary queries
    *  in that route to get the data from the database.
    => send along res.json(profile) as the final output of the route.
    => now if we send a get request to http://localhost:5000/api/profile/me
    *  it will throw the message "No token, authorization denied."
    => But if we send the login post request and get the token and add
    *  it as a x-auth-token header to that http://localhost:5000/api/profile/me
    *  and send the get request, it will throw the message "There is no profile for this user."
    *  because we don't have a profile for that user yet. We just created
    *  this route to pull out the already entered data on a user profile.
    => saving http://localhost:5000/api/profile/me post request in postman.
    => in the next lecture we need to create a post route to actually create the
    *  user profile and populate it with profile data.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   GET api/profile
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => res.send("Profile route"));
+// router.get("/", (req, res) => res.send("Profile route"));
+
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    // This user: here is user: { type: mongoose.Schema.Types.ObjectId, ref: "user", },
+    // on Profile.js model file. The req.user.id is coming alongside with the token
+    // attached to the headers of the get request to this route.
+    // "user" on .populate("user") is the user model we created on User.js file.
+    // It will add both name and the avatar fields to the const profile to.
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: "There is no profile for this user." });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
