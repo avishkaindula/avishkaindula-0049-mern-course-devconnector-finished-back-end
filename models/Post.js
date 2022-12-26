a7345844a05766239c50d1d971a5e889ee7c5f47
Avishka Indula <avishkainduladev@gmail.com>
2022-12-26 18:51:06 +0530

    Creating The Post Model
    => creating the Post.js model
    => adding the avatar: and name: (username) as fields to this model
    => we add those values as separate fields because we need to
    *  provide the users the ability to keep the posts even if they
    *  deleted their accounts.
    => creating likes and comments as arrays in schema because it might
    *  be assigned more than one object.

---
+++ models/Post.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      // This will hold the name of the user who liked the post
+      // A single user can only like a certain post once.
+    },
+    // This will include a array of users who liked some post.
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
