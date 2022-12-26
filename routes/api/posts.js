5652a40ef8eedbe016d7938939ecd0e0c71ff3c8
Avishka Indula <avishkainduladev@gmail.com>
2022-12-26 22:22:17 +0530

    Get & Delete Post Routes
    => creating a router.get("/") requests to get all posts.
    => but we need to be logged in in order to view the posts.
    => so we need to add the jwt web token of any user to the get request.
    => res.json(posts); :- this will fetch all posts created by all users.
    => sending a get request to http://localhost:5000/api/posts
    => creating a route to get a post by id. It will fetch a single
    *  post of created by a certain user.
    *  router.get("/:id"); :- we need to pass along an id of a post to the
    => request url and req.params.id will extract that id from that request url
    => sending a get request to http://localhost:5000/api/posts/63a9c19c72185c886987761d
    *  and get data of a single post.
    => creating the route to delete a post : router.delete("/:id");
    => await post.remove(); in that route.
    => sending a delete request to http://localhost:5000/api/posts/63a9c19c72185c886987761d
    => 63a9c19c72185c886987761d is the id of the post. and we need to add the jwt token
    *  of the user that's created this profile to the header.
    => this will now delete the post from the database.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -44,4 +44,70 @@ router.post(
   }
 );
 
+// @route   POST api/posts
+// @desc    Get all posts
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    // .sort({ date: -1 }) will sort and get the most resent posts first.
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   POST api/:id
+// @desc    Get post by ID
+// @access  Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check whether the delete request is actually sent from the user who created the post.
+    if (post.user.toString() !== req.user.id) {
+      // This will check the user id stored in the post matches the id of the jwt token
+      // of the logged in user or not. post.user is actually a object id. So first we need
+      // to convert into a string before performing this check.
+      return res.status(401).json("User not authorized");
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
