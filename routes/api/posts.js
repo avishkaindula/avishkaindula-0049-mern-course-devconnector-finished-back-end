f9e8cdffa6513156a766c9050a007d8188c66e43
Avishka Indula <avishkainduladev@gmail.com>
2022-12-26 21:17:01 +0530

    Add Post Route
    => importing { check, validation } and auth middleware to posts.js
    => import Post, Profile and User models to posts.js
    => creating router.post("/"); request
    => saving a post to the database by : const post = await newPost.save();
    => send back the saved post as the json response : res.json(post);
    => the value of a generated jwt token will be deferent from each other.
    *  but all those values has the user id implemented to it.
    => adding content-type : application/json and x-auth-token value
    *  to the http://localhost:5000/api/posts
    => sending a post request to http://localhost:5000/api/posts
    => adding "text" : to the raw body of that post request
    => we can see the _id, text, name, avatar, user, likes, comments, date
    *  fields on the response. name and avatar are fetched from the "user" model.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Posts route"));
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+// This will send the request to http://localhost:5000/api/posts/
+// /api/posts is defined when we import this file to server.js
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      // We're sending the jwt web token along with the request. So we can
+      // extract the user.id from that request.
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        // name: and avatar: will come from the user model.
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
