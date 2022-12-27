5c07b61dfe2205f33bc81e0084ccda9410c54b36
Avishka Indula <avishkainduladev@gmail.com>
2022-12-27 09:10:38 +0530

    Post Like & Unlike Routes
    => creating router.put("/like/:id") route.
    => we're sending along the id of the user who liked the post in
    *  the request url.
    => adding logic to check if a post has already been liked by the same user.
    => post.save() and return res.json(post.likes) as the response.
    => adding Avishka Indula 's jwt token to the headers and send a
    *  put request to http://localhost:5000/api/posts/like/63aa638b53ca316d25f6d343
    => it will record that the Avishka Indula user has liked the post 63aa638b53ca316d25f6d343
    => if we send this same request again it will throw the error "Post already liked"
    => now this like is stored in the post as an array.
    *        "likes": [
    *            {
    *                "user": "63a5b686e910c0a6a4918f98",
    *                "_id": "63aa640353ca316d25f6d348"
    *            }
    *        ],
    => creating router.put("/unlike/:id") route.
    => there's no mechanism to dislike a post. Instead it has the
    *  ability to only remove the like from a already liked post.
    => adding Avishka Indula 's jwt token to the headers and send a
    *  put request to http://localhost:5000/api/posts/like/63aa638b53ca316d25f6d343
    => it will remove the like of Avishka Indula from the likes: array
    *  of that post and return a empty array as the response since no one
    *  else has liked that post yet.
    *       []
    => this is how the empty array is shown on the response on postman.
    => if we send this same request again it will throw the error ""Post has not yet been liked"

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -110,4 +110,71 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+// This is gonna be a put request because we're technically updating an already
+// existing post object with a comments array.
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked by the same user
+    // because we don't wanna allow the same user to like the same post twice.
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    // The easy way to think about the above if statement is like following
+    // if (post.likes.filter(likeByCertainUser).length > 0) {}
+
+    post.likes.unshift({ user: req.user.id });
+    // post.like will add data as arrays to the likes: object we created on the Posts model in Posts.js file.
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Like a post
+// @access  Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the logged in user hasn't liked the post yet. Because there's
+    // no point in sending a dislike request to a post that the user hasn't even liked yet.
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+    // The easy way to think about the above if statement is like following
+    // if (post.likes.filter(likeByCertainUser).length === 0) {}
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+    // This will remove a like from the like: [] array of a post.
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
